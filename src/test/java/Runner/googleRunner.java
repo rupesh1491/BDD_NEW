ae20791b7faff41dd77f0735d914ab25884b69a2
Rupesh <rupesh.bppimt@gmil.com>
2024-01-16 19:30:19 +0530

    dcd

--- src/test/java/Runner/googleRunner.java
+++ src/test/java/Runner/googleRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/Amzone.feature", // Path to your feature files
     glue = {"stepdefinitions.google"}, // Package where your step definitions are located
-    //tags = "@smoke", // Optional: Only run scenarios with this tag
+    tags = "@smoke", // Optional: Only run scenarios with this tag
     monochrome = true,
    plugin = {"pretty", "html:target/cucumberReports/cucumberReport.html"} // Optional: Define plugins
     
